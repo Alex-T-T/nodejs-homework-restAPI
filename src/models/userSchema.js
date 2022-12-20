f2de3c6eab0dcfe67fdeeab66bd6e9d1685be183
Oleksandr Tolochko <0504Fusion@gmail.com>
2022-12-20 17:54:38 +0200

    Update userSchema.js

--- src/models/userSchema.js
+++ src/models/userSchema.js
@@ -3,26 +3,25 @@ const {handlerError} = require('../utils')
 
 
 const userSchema = new Schema({
-    password: {
-    type: String,
-        required: [true, 'Set password for user'],
+        password: {
+            type: String,
+            required: [true, 'Set password for user'],
         },
-    email: {
-        type: String,
-        required: [true, 'Email is required'],
-        unique: true,
+        email: {
+            type: String,
+            required: [true, 'Email is required'],
+            unique: true,
         },
-    subscription: {
-        type: String,
-        enum: ["starter", "pro", "business"],
-        default: "starter"
+        subscription: {
+            type: String,
+            enum: ["starter", "pro", "business"],
+            default: "starter"
         },
-    token: {
-        type: String,
-        default: null,
+        token: {
+            type: String,
+            default: null,
         }
-    },
-    {
+}, {
     versionKey: false,
     timestamps: true,
     writeConcern: {
