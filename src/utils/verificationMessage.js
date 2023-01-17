29f14d0207b540402c51e8d422271f89f5bd0162
Oleksandr Tolochko <0504Fusion@gmail.com>
2023-01-17 23:41:44 +0200

    Update verificationMessage.js

--- src/utils/verificationMessage.js
+++ src/utils/verificationMessage.js
@@ -4,8 +4,8 @@ const verificationMessage = (email, verificationToken) => {
     const message = {
         to: email,
         subject: 'add email',
-        text: `All will be fine! Click here to verify your email: <a href='${BASE_URL}/api/users/verify/:${verificationToken}' target='_blank' >Click on me</a>`,
-        html: `<h1>All will be fine! Click here to verify your email: <a href='${BASE_URL}/api/users/verify/:${verificationToken}' target='_blank' >Click on me</a></h1>`
+        text: `All will be fine! Click here to verify your email: <a href='${BASE_URL}/api/users/verify/${verificationToken}' target='_blank' >Click on me</a>`,
+        html: `<h1>All will be fine! Click here to verify your email: <a href='${BASE_URL}/api/users/verify/${verificationToken}' target='_blank' >Click on me</a></h1>`
     }
     return message
 };
